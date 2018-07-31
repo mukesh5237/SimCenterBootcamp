970900b5c2ee2709ae33247a68fd3a940d85d93e
NHERI SimCenter <mramanch@eng.ucsd.edu>
2018-07-31 15:31:57 -0700

    MPI

--- Code/Parallel/mpi/pi.c
+++ Code/Parallel/mpi/pi.c
@@ -1,25 +1,44 @@
+#include <mpi.h>
 #include <stdio.h>
-#include <time.h>
+//#include <time.h>
 
-int main(){
+static long int numSteps = 100000;
+
+int main(int argc, char **argv) {
+
+	int numP, procID;
+
+	// the usual mpi initialization
+  	MPI_Init(&argc, &argv);
+  	MPI_Comm_size(MPI_COMM_WORLD, &numP);
+  	MPI_Comm_rank(MPI_COMM_WORLD, &procID);
 	
-	long int numSteps = 100000;
 	double pi = 0; double time = 0;
 	double dX = 1.0/numSteps;
 	double Xi;
 
-	clock_t start = clock();
+	//clock_t start = clock();
 	
-	for (int i = 1; i<=numSteps; i++){
+	int istart = (procID*numSteps/numP)+1;
+	int iend = (procID+1)*numSteps/numP;
+	
+	for (int i = istart; i<=iend; i++){
 		Xi = (i*dX)-(dX/2);
 		pi += dX*(4.0/(1.0+(Xi*Xi)));
 	}
-	printf("pi = %f \n",pi);
 
-	clock_t stop = clock();
-	double elapsed = (double)(stop-start) * 1000.0 / CLOCKS_PER_SEC;
+	int *globalData = NULL;
+
+	MPI_Gather(&pi, numSteps/numP, MPI_INT, globalData, numSteps/numP, MPI_INT, 0, MPI_COMM_WORLD);
+
+	//printf("pi = %f \n",pi);
+
+	//clock_t stop = clock();
+	//double elapsed = (double)(stop-start) * 1000.0 / CLOCKS_PER_SEC;
 	
-	printf("Time = %f ms \n",elapsed);
+	//printf("Time = %f ms \n",elapsed);
+
+	MPI_Finalize();
 
 	return(0);
 	
